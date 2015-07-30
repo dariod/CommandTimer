d40095c63e086e50eaf870b026989736d4076c28
Dario Dorella <dario.dorella@altair.com>
2015-07-30 15:49:21 -0700

    Polished the implementation
    Now supports iteration on command list
    Now catches keyboard interrupt (this is buggy, though)
    Corrected a hang caused by Popen,wait(): substituted with
    Popen.comunicate()

--- CommandTimer.py
+++ CommandTimer.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
 import subprocess
 import time
+import signal
+import sys
 
 numberOfTries=5
-cmd=['/bin/sleep','2']
-
-
-
+cmds=[['/bin/sleep','2'],
+    ['/bin/sleep','1'],
+    ['/bin/sleep','3']
+    ]
 
 def avg(list):
     sum = 0
     for elm in list:
         sum += elm
     return(sum/(len(list)*1.0))
-
 def min(list):
     min = list[0]
     for elm in list[1:]:
         if elm < min:
             min = elm
     return(min)
-            
 def max(list):
     max = list[0]
     for elm in list[1:]:
@@ -28,20 +28,27 @@ def max(list):
             max = elm
     return(max)
 
-elapsedTimes=[]
-for i in range(numberOfTries):
-    print "Iteration %d..." % (i)
-    try:
-        startTime=time.time()
-        cmdHandle=subprocess.Popen(cmd,shell=False,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    except OSError:
-        print "Cannot exeucte %s" % (cmd)
+for cmd in cmds:
+    elapsedTimes=[]
+    for i in range(numberOfTries):
+        try:
+            startTime=time.time()
+            cmdHandle=subprocess.Popen(cmd,shell=False,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            
+            cmdHandle.communicate()
+            endTime=time.time()
+            elapsedTimes.append(endTime - startTime)
+            
+        except KeyboardInterrupt:
+            sys.stderr.write("Received Ctrl-C\n")
+            cmdHandle.kill()
+            break
+        except OSError:
+            sys.stderr.write("Cannot execute %s" % (cmd))
+        except:
+            sys.stderr.write("Problems executing %s" % (cmd))
         
-    rtn=cmdHandle.wait()
-    endTime=time.time()
-    elapsedTimes.append(endTime - startTime)
-    
-print "Runtimes:"
-print "Minimum: %8.4f" % (max(elapsedTimes))
-print "Maximum: %8.4f" % (min(elapsedTimes))
-print "Average: %8.4f" % (avg(elapsedTimes))
+    sys.stdout.write("Runtimes summary for %d runs of %s:\n" % (len(elapsedTimes)," ".join(cmd)))
+    sys.stdout.write("Minimum: %8.4fs\n" % (min(elapsedTimes)))
+    sys.stdout.write("Maximum: %8.4fs\n" % (max(elapsedTimes)))
+    sys.stdout.write("Average: %8.4fs\n" % (avg(elapsedTimes)))
